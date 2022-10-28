021395d34d88ca5d31e18db8cc67e6f24fb16839
HanPham <petphamhandc@gmail.com>
2022-10-28 17:25:31 +0700

    L11-m4-Web Service

---
+++ src/main/java/com/codegym/service/blog/IBlogService.java
@@ -0,0 +1,8 @@
+package com.codegym.service.blog;
+
+
+import com.codegym.model.Blog;
+import com.codegym.service.IGeneralService;
+
+public interface IBlogService extends IGeneralService<Blog> {
+}
